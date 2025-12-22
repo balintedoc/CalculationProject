af841189a19775c074139e1f48d46023d792a24a
balintedoc <orosz.balint.gm@gmail.com>
2025-12-22 16:06:48 +0000

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,9 @@
+a = 9
+b = 7
+
+print("{} + {} = {}" . format (a, b, a + b))
+print("{} - {} = {}" . format (a, b, a - b))
+print("{} * {} = {}" . format (a, b, a * b))
+print("{} / {} = {}" . format (a, b, a / b))
+print("{} % {} = {}" . format (a, b, a % b))
+print("Most of the calculations between 9 & 6 are here.")
